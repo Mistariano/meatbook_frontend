9e45b70f9b21da5f00ad383c5c41fbca74a5a3be
Mist <hdl730@163.com>
2018-09-08 00:11:03 +0800

    just fucking textarea

--- src/components/editor/MeatBlock.vue
+++ src/components/editor/MeatBlock.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <textarea v-model="meatText" title="text" @keydown.shift.enter="onShiftEnter()" v-show="meatIsEditing"
-              @input="onInput()"></textarea>
-    <div ref="mjElement" v-show="!meatIsEditing" @dblclick="onDoubleClick()" v-html="markedText"></div>
+    <div class="meat-block-textarea-wrapper">
+      <textarea class="meat-block-textarea" ref="meatTextarea" v-model="meatText" title="text"
+              @keydown.shift.enter="onShiftEnter()"
+              v-show="meatIsEditing"
+              @input="onInput()"  style="width: 100%"></textarea>
+    </div>
+    <div class="meat-block-rendered" ref="meatRendered" v-show="!meatIsEditing" @dblclick="onDoubleClick()"
+         v-html="markedText" tabindex="0"></div>
   </div>
 </template>
 <script>
 import marked from 'marked'
+
 export default {
   name: 'MeatBlock',
   props: ['meat'],
   data () {
-    return {}
+    return {
+      textareaRows: 1
+    }
   },
   computed: {
     meatText: {
@@ -19,7 +27,9 @@ export default {
         return this.meat['text']
       },
       set (v) {
-        this.meat['text'] = v
+        if (this.meatIsEditing) {
+          this.meat['text'] = v
+        }
       }
     },
     meatIsEditing: {
@@ -41,6 +51,7 @@ export default {
         window.MathJax.Hub
       ])
       this.meatIsEditing = false
+      this.renderMathJax()
     },
     onDoubleClick: function () {
       this.meatIsEditing = true
@@ -53,7 +64,7 @@ export default {
     // referenced from the source code of vue-mathjax
     // https://github.com/justforuse/vue-mathjax.git
     renderContent () {
-      this.$refs.mjElement.htmlContent = this.markedText
+      this.$refs.meatRendered.htmlContent = this.markedText
     },
     renderMathJax () {
       this.renderContent()
@@ -69,25 +80,44 @@ export default {
           // we use CSS to left justify single line equations in code cells.
           displayAlign: 'center',
           'HTML-CSS': {
-            styles: { '.MathJax_Display': { margin: 0 } },
-            linebreaks: { automatic: true }
+            styles: {'.MathJax_Display': {margin: 0}},
+            linebreaks: {automatic: true}
           }
         })
         window.MathJax.Hub.Queue([
           'Typeset',
           window.MathJax.Hub,
-          this.$refs.mjElement
+          this.$refs.meatRendered
         ])
       }
     }
   },
   watch: {
     meatText () {
-      this.renderMathJax()
+      this.$refs.meatTextarea.style.height = this.$refs.meatTextarea.scrollHeight + 'px'
     }
   }
 }
 </script>
 <style scoped>
+  .meat-block-textarea-wrapper {
+    background: #f3f3f3;
+    border: gray 1px solid;
+    border-bottom: none;
+  }
+  .meat-block-textarea {
+    background: transparent;
+    overflow: hidden;
+    resize: none;
+    padding: 0;
+    border: none;
+    margin: 10px;
+  }
 
+  .meat-block-rendered:focus {
+    border: blue 1px solid;
+    outline: 0;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, .6);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075), 0 0 8px rgba(102, 175, 233, .6)
+  }
 </style>
